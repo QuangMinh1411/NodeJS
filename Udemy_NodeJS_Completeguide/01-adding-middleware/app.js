5e33cdbe33d9f11936b26258a77705e7f2413213
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-06-16 10:53:19 +0700

    Express,routes,static

--- Udemy_NodeJS_Completeguide/01-adding-middleware/app.js
+++ Udemy_NodeJS_Completeguide/01-adding-middleware/app.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add</button></form>"
-  );
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
-});
-
 app.listen(3000);
 // const server = http.createServer(app);
 
